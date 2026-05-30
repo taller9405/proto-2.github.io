578a22c0209434db4f192d71ee8b6f26fc306a56
taller9405 <ej745009405@gmail.com>
2026-05-30 09:08:37 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function inyeccion_error(){
+    window.alert("Pagina en mantenimiento, intente mas tarde")
+}
+function ubicacion_error(){
+    window.alert("Ups... Algo salio mal, intente mas tarde")
+}
